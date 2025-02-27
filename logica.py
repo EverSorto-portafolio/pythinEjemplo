989cd339b01f5e72a4acd294e9aefffb68502f00
EverSorto <everalfredo.sorto@gmail.com>
2025-02-27 15:37:15 -0600

    Se crean las listas de recibo, usuario y acciones

--- logica.py
+++ logica.py
@@ -13,3 +13,15 @@ recibo.append(["125",  67])
 recibo.append(["126", 500]) 
 recibo.append(["127", 100]) 
 recibo.append(["128", 1000]) 
+
+acciones = list()
+acciones.append("Depositar")
+acciones.append("pago de recibo")
+acciones.append("Retirar")
+
+usuario2 = list()
+usuario2.append(" Alfredo")  
+usuario2.append("12345")          
+usuario2.append(1500)           
+
+
